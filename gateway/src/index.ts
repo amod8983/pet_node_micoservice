9fb5b9bf3ecbd92a990c8227c5f5c07b6fc8c1a9
Amod kumar <amod8983@gmail.com>
2025-07-30 18:16:27 +0530

    feat: made user and auth service accessable via gateway only

--- gateway/src/index.ts
+++ gateway/src/index.ts
@@ -1,11 +1,24 @@
 import express from "express";
 import dotenv from "dotenv";
+import { createProxyMiddleware } from "http-proxy-middleware";
 
 dotenv.config();
 const app = express();
 app.use(express.json());
 
 app.get("/health", (_req, res) => res.send("Gateway is healthy"));
+
+app.use('/auth', createProxyMiddleware({
+    target: 'http://auth-service:3001',
+    changeOrigin: true,
+    pathRewrite: {'^/auth': ''},
+}))
+
+app.use('/user', createProxyMiddleware({
+    target: 'http://user-service:3002',
+    changeOrigin: true,
+    pathRewrite: {'^/path': ''},
+}))
 
 const PORT = process.env.PORT || 8081;
 app.listen(PORT, () => console.log(`Gateway running on port ${PORT}`));
